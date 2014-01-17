27445c9a314abf57403dfea604299d149e5cbaca
Chau Ngo <ngokchau@gmail.com>
2014-01-17 00:35:53 -0800

    revert back to ajax

--- index.php
+++ index.php
@@ -14,14 +14,12 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-sm-8 col-sm-offset-2">
-					<form action="show.php">
-						<div class="input-group">
-							<input type="text" class="form-control" name='playerName' id='playerName' placeholder='name'>
-							<span class="input-group-btn">
-								<button class="btn btn-success" type="submit">Search</button>
-							</span>
-						</div>
-					</form>
+					<div class="input-group">
+						<input type="text" class="form-control" name='playerName' id='playerName' placeholder='name'>
+						<span class="input-group-btn">
+							<button class="btn btn-success" type="button" onclick="search()">Search</button>
+						</span>
+					</div>
 				</div>
 			</div>
 			<br>
