3892731e158f8ab0153ce1924189dd64e8fc7229
Chau Ngo <ngokchau@gmail.com>
2014-01-18 11:05:29 -0800

    Add port number

--- config/connect.php
+++ config/connect.php
@@ -1,11 +1,12 @@
 <?php	
 	$user = 'root';
 	$password = 'beta-zeta-omega';
+	$port = 3306;
 	$host = 'informatics.ckup5zfedqvi.us-west-2.rds.amazonaws.com';
 	$db = 'nba_stats';
 
 	try {
-		$dbh = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+		$dbh = new PDO("mysql:host=$host;port=$portdbname=$db", $user, $password);
 	} catch (PDOException $e) {
 		die('Could not connect to database: '.$e->getMessage());
 	}
