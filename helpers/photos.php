f969cfe3ec108b215700686f23a862d0060128b2
Chau Ngo <ngokchau@gmail.com>
2014-01-20 11:31:44 -0800

    reorganize

--- helpers/photos.php
+++ helpers/photos.php
@@ -1,12 +1,12 @@
 <?php 
-	require_once('config/connect.php');
-	require_once('controllers/playerController.php');
+	require_once('../config/connect.php');
+	require_once('../controllers/playerController.php');
 
 	foreach($players as $k => $v) {
 		$name = str_replace('.', '', str_replace(' ', '_', strtolower($players[$k]->getPlayerName())));
 		$url = "http://i.cdn.turner.com/nba/nba/.element/img/2.0/sect/statscube/players/large/$name.png";
 		$contents = file_get_contents($url);
-		$savePath = "assets/images/$name.png";
+		$savePath = "../assets/images/$name.png";
 		sleep(1);
 		if(file_put_contents($savePath, $contents) != 0) 
 			echo "$name is saved"."<br>";
