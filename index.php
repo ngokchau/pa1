8c4026de0aa0c7cef801db26eae7b6aa26a407e4
Chau Ngo <ngokchau@gmail.com>
2014-01-20 11:48:09 -0800

    center logo

--- index.php
+++ index.php
@@ -13,10 +13,11 @@
 
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-4 col-sm-2 col-sm-offset-2">
+				<div class="col-xs-4 col-sm-2 col-xs-offset-4 col-sm-offset-5">
 					<img src="assets/images/nba_logo.jpg" alt="NBA Logo" />
 				</div>
 			</div>
+			<br>
 			<br>
 			<div class="row">
 				<div class="col-xs-12 col-sm-8 col-sm-offset-2">
