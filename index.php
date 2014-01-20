2ed5af27d6901265928189870f514c062d286010
Chau Ngo <ngokchau@gmail.com>
2014-01-20 11:46:28 -0800

    change logo size for xs

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-2 col-sm-2 col-sm-offset-2">
+				<div class="col-xs-4 col-sm-2 col-sm-offset-2">
 					<img src="assets/images/nba_logo.jpg" alt="NBA Logo" />
 				</div>
 			</div>
