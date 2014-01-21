0059cb306ae39ae1ba187e6fb8eb6b462567004b
Chau Ngo <ngokchau@gmail.com>
2014-01-20 18:59:54 -0800

    remove old pdo

--- config/connect.php
+++ config/connect.php
@@ -5,12 +5,8 @@
 	$host = 'informatics.ckup5zfedqvi.us-west-2.rds.amazonaws.com';
 	$db = 'nba_stats';
 
-	// try {
-	// 	$dbh = new PDO("mysql:host=$host;port=$port;dbname=$db;", $user, $password);
-	// } catch (PDOException $e) {
-	// 	die('Could not connect to database: '.$e->getMessage());
-	// }
 	$playerName = $_GET['playerName'];
+	
 	try {
 		$dbh = new PDO("mysql:host=$host;port=$port;dbname=$db;", $user, $password);
 		$stmt = $dbh->prepare("SELECT * FROM players WHERE playerName LIKE :playerName");
