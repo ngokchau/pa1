40ef8bcf3a3e1b9c942359b2f719f886b78e7fa5
Chau Ngo <ngokchau@gmail.com>
2014-03-01 23:32:39 -0800

    JSONP

--- show-json.php
+++ show-json.php
@@ -12,25 +12,26 @@
 	// 	echo json_encode(array());
 	// }
 
+	$jsonpResult = array();
+
+	foreach($players as $k => $v) {
+		$jsonpResult[$players[$k]->getPlayerName()] = {
+			"gp": $players[$k]->getPlayerGp(),
+			"fgp": $players[$k]->getPlayerFgp(),
+			"tpp": $players[$k]->getPlayerTpp(),
+			"ftp": $players[$k]->getPlayerFtp(),
+			"ppg": $players[$k]->getPlayerPpg()
+		};
+	}
+
 	if(array_key_exists('callback', $_GET)) {
 		header('Content-Type: text/javascript; charset=utf8');
 		header("Access-Control-Max-Age: 3628800");
 		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-		$jsonpResult = array();
-
-		foreach($players as $k => $v) {
-			$jsonpResult[$players[$k]->getPlayerName()] = {
-				"gp": $players[$k]->getPlayerGp(),
-				"fgp": $players[$k]->getPlayerFgp(),
-				"tpp": $players[$k]->getPlayerTpp(),
-				"ftp": $players[$k]->getPlayerFtp(),
-				"ppg": $players[$k]->getPlayerPpg()
-			};
-		}		
 		
 		$callback = $_GET['callback'];
 		echo $callback.'('.json_encode($jsonpResult).');';
 	} else {
-		echo json_encode($players);
+		echo json_encode($jsonpResult);
 	}
 ?>
