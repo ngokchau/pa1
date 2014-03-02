17d91896f69fe3c2ae7c64df5d5629a0fc498511
Chau Ngo <ngokchau@gmail.com>
2014-03-01 23:41:40 -0800

    JSONP

--- show-json.php
+++ show-json.php
@@ -30,7 +30,7 @@
 		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 		
 		$callback = $_GET['callback'];
-		echo $callback.'('.json_encode($jsonpResult).');';
+		echo $callback.'('.$jsonpResult.');';
 	} else {
 		echo json_encode($jsonpResult);
 	}
