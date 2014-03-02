0b84e908de60a2d46594353f4f1d6d18d5aa7249
Chau Ngo <ngokchau@gmail.com>
2014-03-01 23:05:37 -0800

    JSONP

--- show-json.php
+++ show-json.php
@@ -4,34 +4,22 @@
 
 
 <?php
-	if($players) {
-		$jsonResults = array();
+	// if($players) {
+	// 	$jsonResults = array();
 		
-		echo json_encode($players);
+	// 	echo json_encode($players);
+	// } else {
+	// 	echo json_encode(array());
+	// }
+
+	if(array_key_exists('callback', $_GET)) {
+		header('Content-Type: text/javascript; charset=utf8');
+		header("Access-Control-Max-Age: 3628800");
+		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+		$callback = $_GET['callback'];
+		echo $callback.'('.$players.');';
 	} else {
-		echo "<div class='col-xs-6 col-md-4'>";
-		echo "<div class='panel panel-default card'>";
-		echo "<div class='panel-heading'>";
-		echo "<h5 class='panel-title text-center'>No Result</h5>";
-		echo "</div>";
-
-		echo "<div class='panel-body'>";
-		echo "<div class='dp col-xs-8 col-xs-offset-2'>";
-		echo "<img src='assets/images/no_result.png' alt='no result' class='img-circle'>";
-		echo "</div>";
-		echo "</div>";
-
-		echo "<table class='table table-bordered'>";
-		echo "<tr><th>GP</th><th>FGP</th><th>TPP</th><th>FTP</th><th>PPG</th></tr>";
-		echo 	"<tr><td>-".
-					"</td><td>-".
-					"</td><td>-".
-					"</td><td>-".
-					"</td><td>-".
-					"</td></tr>";
-		echo "</table>";
-		
-		echo "</div>";
-		echo "</div>";
+		echo $players;
 	}
 ?>
