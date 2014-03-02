27fd33b6089cde8cd5bc7702a99f34b101acc726
Chau Ngo <ngokchau@gmail.com>
2014-03-01 23:29:07 -0800

    JSONP

--- show-json.php
+++ show-json.php
@@ -16,9 +16,20 @@
 		header('Content-Type: text/javascript; charset=utf8');
 		header("Access-Control-Max-Age: 3628800");
 		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+		$jsonpResult = array();
 
+		foreach($players as $k => $v) {
+			$jsonpResult[$players[$k]-getPlayerName()] = {
+				"gp": $players[$k]->getPlayerGp(),
+				"fgp": $players[$k]->getPlayerFgp(),
+				"tpp": $players[$k]->getPlayerTpp(),
+				"ftp": $players[$k]->getPlayerFtp(),
+				"ppg": $players[$k]->getPlayerPpg()
+			};
+		}		
+		
 		$callback = $_GET['callback'];
-		echo $callback.'('.$players.');';
+		echo $callback.'('.$jsonpResult.');';
 	} else {
 		echo json_encode($players);
 	}
